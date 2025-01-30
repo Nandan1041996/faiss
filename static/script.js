7b6d870f1bdce233b9d11b1eced0c132b8bf3acc
Nandan1041996 <mayurnandanwar@ghcl.co.in>
2025-01-30 12:48:32 +0530

    Delete static directory

--- static/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("query-form").addEventListener("submit", function (e) {
-    const responseSection = document.getElementById("response");
-    responseSection.textContent = "Processing your query, please wait...";
-});
